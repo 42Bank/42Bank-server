7e888787e28f7833e5a22d40a19500d9c85d666c
hysimok <jeonghy95@gmail.com>
2020-12-16 20:51:46 +0900

    add models in admin pages

--- bank42/crawler/admin.py
+++ bank42/crawler/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User
+from .models import *
 
 admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(AchvList)
+admin.site.register(Achved)
+admin.site.register(Shop)
+admin.site.register(Notice)
