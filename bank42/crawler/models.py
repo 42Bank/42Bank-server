ffd6869baf05b30e37de7d9733fa9b5dd5020bc9
Jiyoon Hur <60052127+jiyoon1156@users.noreply.github.com>
2020-12-16 20:22:24 +0900

    [ADD]: rest of models (#2)
    * [ADD]: order, acheivement list, achieved, shop, notice models
    
    * [ADD] gitignore pycache
    
    * [ADD]: models
    
    * [REMOVE]: cache

--- bank42/crawler/models.py
+++ bank42/crawler/models.py
@@ -7,3 +7,31 @@ class User(models.Model):
     user_id = models.CharField(max_length=30)
     photo = models.URLField(blank=True)
     cur_wallet = models.SmallIntegerField(default=0)
+
+class Order(models.Model):
+    user_id = models.CharField(max_length=30)
+    date = models.DateTimeField('checked date')
+    content = models.CharField(max_length=30)
+    get_wallet = models.SmallIntegerField(default=0)
+    spend_wallet = models.SmallIntegerField(default=0)
+    cur_wallet = models.SmallIntegerField(default=0)
+
+class AchvList(models.Model):
+    title = models.CharField(max_length=30)
+    wallet_p = models.SmallIntegerField(default=0)
+
+class Achved(models.Model):
+    user_id = models.CharField(max_length=30)
+    achievement = models.CharField(max_length=30)
+    date = models.DateTimeField('checked date')
+
+class Shop(models.Model):
+    product_id = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    price = models.SmallIntegerField(default=0)
+    photo = models.URLField(blank=True)
+
+class Notice(models.Model):
+    date = models.DateTimeField('event date')
+    title = models.CharField(max_length=30)
+    price = models.CharField(max_length=30)
