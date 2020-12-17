e2297ba3ca632e9e1d86451e94eee62336e4adaa
민아영 <min-ayeong@min-ayeong-ui-MacBookPro.local>
2020-12-17 21:29:37 +0900

    get access token

--- bank42/crawler/views.py
+++ bank42/crawler/views.py
@@ -1,12 +1,12 @@
-# import pprint
-# import requests
+import requests
 from django.shortcuts import render
-# import webbrowser
 from django.http import HttpResponse
+import jsons
 
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view, parser_classes
+from requests_oauthlib import OAuth2Session
 
 from rest_framework import viewsets
 from .serializers import *
@@ -33,36 +33,33 @@ class NoticeView(viewsets.ModelViewSet):
 
 # Create your views here.
 
-# pp = pprint.PrettyPrinter(indent=4)
-# API_URL = "https://api.intra.42.fr/"
-# uid = 'd7e39c6ab1b81ead0f83a923fcebe14c0778a1e16fd7eb24807e377a6e8ab113'
-# secret = '4fff1b6417d07be99bc2c48649dcb7e328ac0b3bfb58b24ab9d699a3d2cb9a42'
+uid = '0dfca8b2cffc7928d7e207b36879659b90f37e90b90c5e521077ad07ee20a113'
+secret = '772317d483a5965bb6cf89cce6c14c46b43111d8f119d5e1668f0f512f8c5904'
+CALLBACK = "https%3A%2F%2Fprofile.intra.42.fr"
+API_URL = "https://api.intra.42.fr/"
+authorize_url = 'https://api.intra.42.fr/oauth/authorize'
+redirect_uri = 'http://localhost:3000/main'
+scope = 'public'
+
 
 @api_view(['POST'])
 @parser_classes((JSONParser,))
 def index(request):
-    foo = request.data.get('code')
-    print(foo)
-    return JsonResponse({
-        'code': foo,
-    })
+    code = request.data.get('code')
+    data = {
+        'grant_type': 'authorization_code',
+        'client_id': uid,
+        'client_secret': secret,
+        'code': code,
+        'redirect_uri': redirect_uri
+    }
+    r = requests.post("https://api.intra.42.fr/oauth/token", data)
+    json = r.json()
+    token = json['access_token']
+    me_url = API_URL + "v2/me"
+    print(token)
+    headers = {'Authorization': 'Bearer ' + token}
+    r = requests.get(me_url, headers=headers)
+    print("Me:", r.status_code, r.reason)
 
-# def get_token(self):
-#     CALLBACK = "https%3A%2F%2Fprofile.intra.42.fr"
-#     API_URL = "https://api.intra.42.fr/"
-#     # CALLBACK = "http%3A%2F%2F127.0.0.1%3A8000"
-#     webbrowser.open(
-#         API_URL + "oauth/authorize?client_id=" + uid + "&redirect_uri=" + CALLBACK + "&response_type=code&scope=public%20forum")
-#     code = input("Enter code here:")
-#     data = {
-#         'grant_type': 'authorization_code',
-#         'client_id': uid,
-#         'client_secret': secret,
-#         'code': code,
-#         'redirect_uri': 'https://profile.intra.42.fr'
-#         # 'redirect_uri': 'http://127.0.0.1:8000'
-#     }
-#     r = requests.post("https://api.intra.42.fr/oauth/token", data)
-#     print("Token:", r.content)
-#     json = r.json()
-#     self.token = json['access_token']
+    return HttpResponse(json['access_token'])
